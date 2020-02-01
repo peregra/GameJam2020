417d437bba6f9a95026bbb1fb83f31e5e957f497
PeterHellmuth <peterhellmuth@gmail.com>
2020-01-31 20:24:47 -0600

    FPS template

---
+++ Source/Magnet/MagnetGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "MagnetGameMode.h"
+#include "MagnetHUD.h"
+#include "MagnetCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMagnetGameMode::AMagnetGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AMagnetHUD::StaticClass();
+}
---
+++ Source/Magnet/MagnetHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "MagnetHUD.generated.h"
+
+UCLASS()
+class AMagnetHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	AMagnetHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
